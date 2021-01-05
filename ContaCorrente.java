750e6a577ab7ef2b8e5e46286747e3347ae12f52
Dtjak <58142016+Dtjak@users.noreply.github.com>
2021-01-05 00:28:53 -0300

    taxa de operação -> valor fixo

---
+++ ContaCorrente.java
@@ -0,0 +1,24 @@
+package Parte1;
+
+public class ContaCorrente extends ContaBancaria {
+
+    private double taxa_op;
+
+    public ContaCorrente(double taxa_op, int num_conta, double saldo) {
+        super(num_conta, saldo);
+        this.taxa_op = taxa_op;
+    }
+
+    public void taxa() {
+        setSaldo(getSaldo() - getTaxa_op());
+    }
+
+    public double getTaxa_op() {
+        return taxa_op;
+    }
+
+    public void setTaxa_op(double taxa_op) {
+        this.taxa_op = taxa_op;
+    }
+
+}
