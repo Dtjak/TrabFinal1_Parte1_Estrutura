66c6f99bea2946db327b3259581cebd7e10be5c4
Dtjak <58142016+Dtjak@users.noreply.github.com>
2021-01-14 14:58:37 -0300

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -4,60 +4,64 @@ import java.util.ArrayList;
 
 public class Banco {
 
-    private ArrayList<ContaPoupanca> contas_p = new ArrayList<>(10);
     private ArrayList<ContaCorrente> contas_c = new ArrayList<>(10);
-
-    public Banco() {}
+    private ArrayList<ContaPoupanca> contas_p = new ArrayList<>(10);
+    
+    public Banco(ArrayList<ContaCorrente> contas_c, ArrayList<ContaPoupanca> contas_p) {
+        this.contas_c = contas_c;
+        this.contas_p = contas_p;
+    }
 
     public void inserirCC(ContaCorrente conta) {
-        getContasC().add(conta);
+        getContas_c().add(conta);
     }
-    
+
     public void inserirCP(ContaPoupanca conta) {
-        getContasP().add(conta);
+        getContas_p().add(conta);
     }
 
     public void removerCC(ContaCorrente conta) {
-        getContasC().remove(conta);
+        getContas_c().remove(conta);
     }
-    
+
     public void removerCP(ContaPoupanca conta) {
-        getContasP().remove(conta);
+        getContas_p().remove(conta);
     }
 
     public ContaCorrente procurarContaC(int num_conta) {
         ContaCorrente conta = null;
-        for (int i = 0; i < getContasC().size(); i++) {
-            if (getContasC().get(i).getNum_conta() == num_conta) {
-                conta = getContasC().get(i);
+        for (int i = 0; i < getContas_c().size(); i++) {
+            if (getContas_c().get(i).getNum_conta() == num_conta) {
+                conta = getContas_c().get(i);
             }
         }
         return conta;
     }
-    
+
     public ContaPoupanca procurarContaP(int num_conta) {
         ContaPoupanca conta = null;
-        for (int i = 0; i < getContasP().size(); i++) {
-            if (getContasP().get(i).getNum_conta() == num_conta) {
-                conta = getContasP().get(i);
+        for (int i = 0; i < getContas_p().size(); i++) {
+            if (getContas_p().get(i).getNum_conta() == num_conta) {
+                conta = getContas_p().get(i);
             }
         }
         return conta;
     }
 
-    public ArrayList<ContaCorrente> getContasC() {
-        return contas_c;
-    }
-
-    public void setContasC(ArrayList<ContaCorrente> contas) {
-        this.contas_c = contas;
-    }
-    
-    public ArrayList<ContaPoupanca> getContasP() {
+    public ArrayList<ContaPoupanca> getContas_p() {
         return contas_p;
     }
 
-    public void setContasP(ArrayList<ContaPoupanca> contas) {
-        this.contas_p = contas;
+    public void setContas_p(ArrayList<ContaPoupanca> contas_p) {
+        this.contas_p = contas_p;
     }
+
+    public ArrayList<ContaCorrente> getContas_c() {
+        return contas_c;
+    }
+
+    public void setContas_c(ArrayList<ContaCorrente> contas_c) {
+        this.contas_c = contas_c;
+    }
+
 }
