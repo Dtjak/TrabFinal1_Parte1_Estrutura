43de1c48a81561671a9b376d367502d1193943d7
Dtjak <58142016+Dtjak@users.noreply.github.com>
2021-01-14 14:57:41 -0300

    Update ContaPoupanca.java

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -9,6 +9,30 @@ public class ContaPoupanca extends ContaBancaria {
         this.limite = limite;
     }
 
+    @Override
+    public Boolean sacar(double valor) {
+        if (getSaldo() + getLimite() > valor) {
+            setSaldo(getSaldo() - valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void depositar(double valor) {
+        setSaldo(getSaldo() + valor);
+    }
+
+    @Override
+    public void transferir(double valor, ContaBancaria conta) {
+        if (sacar(valor) == true) {
+            conta.depositar(valor);
+        } else {
+            System.out.println("Saldo insuficiente para transferência");
+        }
+    }
+
     public double getLimite() {
         return limite;
     }
