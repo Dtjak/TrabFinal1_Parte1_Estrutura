e0df98ed0ff7cea184e18f499f2182c3df7c34ec
Dtjak <58142016+Dtjak@users.noreply.github.com>
2021-01-16 00:31:11 -0300

    Update Relatorio.java

--- Relatorio.java
+++ Relatorio.java
@@ -9,10 +9,17 @@ public class Relatorio {
     public Relatorio() {}
     
     public void mostrarDadosCC(ContaCorrente conta) {
-        System.out.printf("DADOS DA CONTA\nNúmero da Conta: %d\nSaldo: R$ %.2f\nLimite: R$ %.2f\n",conta.getNum_conta(),conta.getSaldo(),conta.getTaxaDeOperacao());
+        System.out.printf("DADOS DA CONTA\n"
+                + "Número da Conta: %d\n"
+                + "Saldo: R$ %.2f\n"
+                + "Limite: R$ %.2f\n",conta.getNum_conta(),conta.getSaldo(),conta.getTaxaDeOperacao());
+        //printf apenas para imprimir valores em reais com duas casas decimais 
     }
     
     public void mostrarDadosCP(ContaPoupanca conta) {
-        System.out.printf("DADOS DA CONTA\nNúmero da Conta: %d\nSaldo: R$ %.2f\nLimite: R$ %.2f\n",conta.getNum_conta(),conta.getSaldo(),conta.getLimite());
+        System.out.printf("DADOS DA CONTA\n"
+                + "Número da Conta: %d\n"
+                + "Saldo: R$ %.2f\n"
+                + "Limite: R$ %.2f\n",conta.getNum_conta(),conta.getSaldo(),conta.getLimite());
     }
 }
