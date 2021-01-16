84252dd7229efa094049fdb739009728a87dc2a6
Dtjak <58142016+Dtjak@users.noreply.github.com>
2021-01-16 00:31:58 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -11,8 +11,8 @@ public class ContaCorrente extends ContaBancaria {
 
     @Override
     public Boolean sacar(double valor) {
-        if (getSaldo() > valor + getTaxaDeOperacao()) {
-            setSaldo(getSaldo() - valor - getTaxaDeOperacao());
+        if (getSaldo() > valor + getTaxaDeOperacao()) { //deve haver saldo para descontar a taxa de operação 
+            setSaldo(getSaldo() - valor - getTaxaDeOperacao()); //desconta valor de saque e taxa de operação
             return true;
         } else {
             return false;
@@ -21,13 +21,13 @@ public class ContaCorrente extends ContaBancaria {
 
     @Override
     public void depositar(double valor) {
-        setSaldo(getSaldo() + valor - getTaxaDeOperacao());
+        setSaldo(getSaldo() + valor - getTaxaDeOperacao()); //soma o valor depositado e desconta a taxa de operação
     }
 
     @Override
     public void transferir(double valor, ContaBancaria conta) {
-        if (sacar(valor) == true) {
-            conta.depositar(valor);
+        if (sacar(valor) == true) { //verifica se é possível fazer saque e, se for possível,
+            conta.depositar(valor);  //deposita o valor correspondente na conta direcionada
         } else {
             System.out.println("Saldo insuficiente para transferência");
         }
